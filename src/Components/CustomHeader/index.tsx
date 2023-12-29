530ce758a15b7b34816a9acdd2801f073925fc90
Punit Soni <punit.soni33@gmail.com>
2023-12-29 14:33:48 +0530

    product hunt label added
    build fail fixed
    
    header ui issue fixed

--- src/Components/CustomHeader/index.tsx
+++ src/Components/CustomHeader/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Group, Tooltip, rem, ActionIcon, Text } from '@mantine/core';
+import { Container, Group, Tooltip, rem, ActionIcon, Text, Anchor } from '@mantine/core';
 import { appName } from '../../utils';
 import { FaRegHandshake } from 'react-icons/fa';
 import { IoSettingsOutline } from "react-icons/io5";
@@ -12,7 +12,7 @@ const iconHeight = 20;
 const iconWidth = 20;
 
 export default function CustomHeader() {
-    
+
     const supportContext = useContext(SupportContext);
     const configContext = useContext(ConfigurationContext);
 
@@ -51,13 +51,18 @@ export default function CustomHeader() {
     return (
         <header className={classes.header}>
             <Container size="lg" className={classes.inner}>
-
                 <Group gap={8}>
                     <MdPhotoSizeSelectLarge style={{ padding: 0, width: rem(32), height: rem(32), color: 'var(--mantine-color-blue-6)' }} />
-                    <Text fz="lg" fw="bold" c="blue">{appName}</Text>
+                    <Text visibleFrom="xs" fz="lg" fw="bold" c="blue">{appName}</Text>
                 </Group>
 
                 <Group gap={10}>
+                    <Anchor mt={6} href="https://www.producthunt.com/posts/downsize?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-downsize"
+                        target="_blank">
+                        <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=431834&theme=neutral"
+                            alt="Downsize - Compress&#0032;your&#0032;images&#0032;to&#0032;save&#0032;your&#0032;storage | Product Hunt"
+                            width="auto" height="44" />
+                    </Anchor>
                     {items}
                 </Group>
 
